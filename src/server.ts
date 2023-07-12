989c43a1a6a9d2d48b1c9f4ce0485f4112f56453
Pablo Silva <pablo@buybye.com.br>
2023-07-12 08:24:00 -0300

    chore: configured knex sqlite connection

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import fastify from 'fastify'
+import { knex } from './database'
 const app = fastify()
 const port = 3336
+
+app.get('/test', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+  return tables
+})
 
 app.listen(
   {
