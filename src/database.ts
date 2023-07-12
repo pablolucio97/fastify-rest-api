f451b8e715746678e1bebd329072d4d9aad4e265
Pablo Silva <pablo@buybye.com.br>
2023-07-12 08:53:10 -0300

    chore: configured database configuration

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,15 @@
-import { knex as knexSetup } from 'knex'
+import { knex as knexSetup, Knex } from 'knex'
 
-export const knex = knexSetup({
+export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './temp/app.db',
+    filename: './db/app.db',
   },
-})
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
+
+export const knex = knexSetup(config)
