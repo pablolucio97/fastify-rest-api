a9c70a638c740f1f99a9a3ded1327474b12569b7
Pablo Silva <pablo@buybye.com.br>
2023-07-12 08:08:52 -0300

    chore: added lint configuration

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
-import fastify from "fastify";
-
+import fastify from 'fastify'
 const app = fastify()
 const port = 3336
 
-
-app.listen({
-    port
-}, () => {
+app.listen(
+  {
+    port,
+  },
+  () => {
     console.log(`Server running at ${port}`)
-})
+  },
+)
