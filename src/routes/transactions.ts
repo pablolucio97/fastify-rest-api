37beeb93e5c102393fe4a87e3117d5705a498dd8
Pablo Silva <pablo@buybye.com.br>
2023-07-15 07:34:37 -0300

    feat: created createTransaction route with zod validation

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,11 +1,29 @@
 import { FastifyInstance } from "fastify"
 import { knex } from '../database'
+import { z as Zod } from 'zod'
+import { randomUUID } from 'node:crypto'
 
-export async function transactionsRoutes(app : FastifyInstance){
-    app.get('/test', async () => {
-        const tables = await knex('transactions').select('*')
-          .where('amount', 1000.99)
-        return tables
-      })
+export async function transactionsRoutes(app: FastifyInstance) {
+    app.post('/', async (req, rep) => {
+
+        //SCHEMA FOR ZOD REQUEST BODY VALIDATION
+        const createTransactionBodySchema = Zod.object({
+            title: Zod.string(),
+            amount: Zod.number(),
+            type: Zod.enum(['credit', 'debit'])
+        })
+
+        //VALIDATES IF THE RECEIVED BODY IS VALID, OTHERWISE THROWS AN ERROR
+        const { title, amount, type } = createTransactionBodySchema.parse(req.body)
+
+        //INSERTS A NEW TRANSACTION INTO TRANSACTIONS
+        await knex('transactions').insert({
+            id: randomUUID(),
+            title,
+            //INSERT THE AMOUNT VALUE AS CREDIT OR DEBIT
+            amount: type === 'credit' ? amount : amount * -1
+        })
+
+        return rep.status(201).send()
+    })
 }
-  
