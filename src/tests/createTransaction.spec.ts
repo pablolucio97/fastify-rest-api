f32157dd9a1d6c87ea5ec889f4644e743c1236f9
Pablo Silva <pablo@buybye.com.br>
2023-07-24 15:54:53 -0300

    tests: created test for listing transactions

--- src/tests/createTransaction.spec.ts
+++ src/tests/createTransaction.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect, beforeAll, afterAll } from 'vitest'
+import { it, expect, beforeAll, afterAll, describe } from 'vitest'
 import supertest from 'supertest'
 import { app } from '../app'
 
@@ -12,13 +12,40 @@ afterAll(async () => {
     await app.close()
 })
 
-test('Allow user to create a new transaction', async () => {
-    const response = await supertest(app.server)
-        .post('/transactions')
-        .send({
-            title: "Freela",
-            amount: 5000,
-            type: "credit"
-        })
-    expect(response.statusCode).toBe(201)
+describe('Transactions', () => {
+    it('Allow user to create a new transaction', async () => {
+        const response = await supertest(app.server)
+            .post('/transactions')
+            .send({
+                title: "Freela",
+                amount: 5000,
+                type: "credit"
+            })
+        expect(response.statusCode).toBe(201)
+    })
+
+    it('should be able to list transactions', async () => {
+
+        const createTransactionResponse = await supertest(app.server)
+            .post('/transactions')
+            .send({
+                title: "Freela",
+                amount: 5000,
+                type: "credit"
+            })
+
+        //READ THE COOKIE FROM createTransactionResponse    
+        const cookies = createTransactionResponse.get('Set-Cookie')
+
+        const listTransactionsResponse = await supertest(app.server)
+            .get('/transactions')
+            //SET THE MANDATORY COOKIE FOR LIST TRANSACTIONS
+            .set('Cookie', cookies)
+        expect(listTransactionsResponse.body.transactions).toEqual([
+            expect.objectContaining({
+                title: "Freela",
+                amount: 5000
+            })
+        ])
+    })
 })
